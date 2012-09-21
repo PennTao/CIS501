be0a613092d492fc125e52ed2d4bea8fb156a176
Tao LEI <whu.eis.lt@gmail.com>
2012-09-20 21:14:53 -0400

    Hw1aQ5.4

--- hw1/simulator.c
+++ hw1/simulator.c
@@ -151,7 +151,10 @@ void simulate(FILE* inputFile, FILE* outputFile)
   {
 	  fprintf(outputFile,"Bit Length: %d	Cnt: %ld\r\n",ItrBitCnt->first, ItrBitCnt->second);
 	  //extraInstruction = extraInstruction + ((floor(ItrBitCnt->first /8) - 1) > 0?(floor(ItrBitCnt->first /8) - 1):0) * ItrBitCnt->second;
-	  printf("ex bit: %d \r\n", floor(ItrBitCnt->first / 8.0) - 1);
+	  if(ItrBitCnt->first >= 9 && ItrBitCnt->first <= 16)
+		  extraInstruction += ItrBitCnt->second;
+	  else if(ItrBitCnt->first >= 17)
+		  extraInstruction += ItrBitCnt->second * 2;
   }
   fprintf(outputFile,"extra instructions: %ld\r\n", extraInstruction);
   for(int i = 0; i < sizeof(instructionClassifier)/sizeof(uint64_t); i++)
